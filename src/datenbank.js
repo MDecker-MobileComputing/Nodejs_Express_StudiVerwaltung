5822359e8d0ac7f9b3f473d59eb18d8257cde357
MDecker-MobileComputing <mide_42b@yahoo.de>
2024-03-01 11:45:18 +0100

    Datenbank mit Studiengaengen wird beim Start geladen

---
+++ src/datenbank.js
@@ -0,0 +1,59 @@
+import { JSONFilePreset } from 'lowdb/node';
+import logging from "logging";
+
+
+const logger = logging.default("datenbank");
+
+const dbDateiName = "db.json"; // diese Datei in .gitignore und Ingore-Liste für nodemon aufnehmen
+
+
+const anfangsDaten =  {
+    "studiengaenge": [
+       {
+        "kurz": "BWL",
+        "lang": "Betriebswirtschaftslehre"
+       },{
+        "kurz": "WING",
+        "lang": "Wirtschaftsingenieurwesen"
+        },{
+        "kurz": "WINF",
+        "lang": "Wirtschaftsinformatik"
+       },{
+        "kurz": "IWMM",
+        "lang": "Irgendwas mit Medien"
+       },{
+        "kurz": "VWL",
+        "lang": "Volkswirtschaftslehre"
+       },{
+        "kurz": "BW",
+        "lang": "Brauereiwesen"
+       },{
+        "kurz": "WBÖ",
+        "lang": "Weinbau und Önologie"
+       },{
+        "kurz": "LUD",
+        "lang": "Ludologie"
+       }
+    ]
+};
+
+
+/* Objekt für Zugriff auf Datenbank. */
+let db = null;
+
+export async function datenbankInitialisieren() {
+
+    db = await JSONFilePreset( dbDateiName, anfangsDaten );
+
+    await db.write();
+    logger.info(`Datenbank mit Datei "${dbDateiName}" initialisiert.`);
+    logger.info(`Anzahl Studiengänge: ${db.data.studiengaenge.length}`);
+}
+
+
+
+
+
+
+
+
---
+++ src/main.js
@@ -0,0 +1,19 @@
+import express from "express";
+import logging from "logging";
+
+import { datenbankInitialisieren } from "./datenbank.js";
+
+
+await datenbankInitialisieren();
+
+
+const logger = logging.default("main");
+
+const app = express();
+
+
+// Server starten
+const PORT_NUMMER = 8080;
+app.listen( PORT_NUMMER,
+    () => { logger.info(`Web-Server lauscht auf Port ${PORT_NUMMER}.\n`); }
+  );
